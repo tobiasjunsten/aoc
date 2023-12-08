deb6def3d1379343e97c669489c7b68dc20b8555
Tobias Junsten <tobias@junsten.se>
2023-12-08 19:25:19 +0100

    Day 8 part1

--- 2023/dart/lib/day8.dart
+++ 2023/dart/lib/day8.dart
@@ -1,4 +1,5 @@
 import 'package:aoc2023/advent_day.dart';
+import 'package:aoc2023/util.dart';
 
 main() => Day8().solve();
 
@@ -7,7 +8,31 @@ class Day8 extends AdventDay {
 
   @override
   part1(String input) {
-    return '';
+    final parts = input.split("\n\n");
+    final instructions = parts[0];
+    final network = parts[1].rows();
+    var nodes = Map.fromIterable(network,
+        key: (v) => v.split(" = ")[0],
+        value: (v) {
+          final lookups = v
+              .split(" = ")[1]
+              .replaceAll("(", "")
+              .replaceAll(")", "")
+              .split(", ");
+          return (lookups[0], lookups[1]);
+        });
+
+    var currentNode = "AAA";
+    var iterations = 0;
+    while (currentNode != "ZZZ") {
+      final position = iterations % instructions.length;
+      final lookup = nodes[currentNode];
+      final instruction = instructions[position];
+      currentNode = instruction == "L" ? lookup?.$1 : lookup?.$2;
+      iterations++;
+    }
+
+    return iterations;
   }
 
   @override
